f0cd90c84fa73faac09efc494e6c88f287668c6a
Lars-Espen Kolberg <larsespen9@gmail.com>
2018-01-11 04:02:43 +0100

    New level, obstacles, tags, scriptchanges to change level, delay on death, delay on levelchange, design changes etc.

--- rocket/Assets/Rocket.cs
+++ rocket/Assets/Rocket.cs
@@ -2,14 +2,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Rocket : MonoBehaviour {
+    // todo fix lighting bug 
     // Game variables
     [SerializeField]float rcsThrust = 100f; // Gjør at vi kan endre verdi i unity ide istedenfor her
     [SerializeField] float mainThrust = 100f; // Gjør at vi kan endre verdi i unity ide istedenfor her
 
     Rigidbody rigidBody;
     AudioSource audiosource;
+
+    // Game state
+    enum State { Alive, Dying, Trancending };
+    State state = State.Alive;
 
 	// Use this for initialization
 	void Start () {
@@ -19,23 +25,40 @@ public class Rocket : MonoBehaviour {
 	
 	// Update is called once per frame
     void Update () {
-        Thrust();
-        Rotate();
+        // todo stop sound on dead
+        if(state == State.Alive)
+        {
+            Thrust();
+            Rotate();
+        }
 	}
     private void OnCollisionEnter(Collision collision)
     {
+        if (state != State.Alive){return;} // gidder ikke å kjøre all koden under hvis vi allerede har dau
         switch (collision.gameObject.tag)
         {
             case "Friendly":
                 print("OK"); // todo remove 
                 break;
             case "Finish":
-                print("Finish"); // todo remove
+                state = State.Trancending;
+                Invoke("LoadNextScene", 1f); // todo lag en serialized filed så vi kan justere denne ved testing
                 break;
             default:
-                print("Dead");
+                state = State.Dying;
+                Invoke("LoadFirstScene", 1f);
                 break;
         }
+    }
+
+    private static void LoadFirstScene()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    private static void LoadNextScene() // todo allow for more than two levels 
+    {
+        SceneManager.LoadScene(1);
     }
 
     private void Rotate()
